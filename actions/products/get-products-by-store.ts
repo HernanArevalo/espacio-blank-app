f6b882040580703d32537be2e234113daacc7ca6
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-08-28 23:22:28 -0300

    update products action with a store filter

--- actions/products/get-products-by-store.ts
+++ actions/products/get-products-by-store.ts
@@ -1,23 +1,63 @@
-import { Product } from "@/interfaces";
+import { Product } from '@/interfaces';
 
-export function GetProductsByStore (): Product[]  {
+export function GetProductsByStore(storeId:number): Product[] {
+  const allProducts = [
+    {
+      name: 'Zapatillas deportivas',
+      productId: 1,
+      stock: 2,
+      storeId: 1,
+      price: 15000,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s',
+    },
+    {
+      name: 'Amoladora INGCO',
+      productId: 2,
+      storeId: 1,
+      stock: 1,
+      price: 20000,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s',
+    },
+    {
+      name: 'Ojotas amarillas',
+      productId: 3,
+      storeId: 1,
+      stock: 2,
+      price: 15000,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s',
+    },
+    {
+      name: 'Crocs Band',
+      productId: 4,
+      storeId: 1,
+      stock: 1,
+      price: 20000,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s',
+    },
+    {
+      name: 'Pantuflas Toy Story',
+      productId: 5,
+      storeId: 2,
+      stock: 2,
+      price: 15000,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s',
+    },
+    {
+      name: 'Bucaneras',
+      productId: 6,
+      stock: 1,
+      storeId: 2,
 
-  return [
-        {
-          name: "Zapatillas deportivas",
-          productId: 4,
-          stock: 2,
-          storeId: 1,
-          price: 15000,
-          image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s'
-        },
-        {
-          name: "Amoladora INGCO",
-          productId: 7,
-          stock: 1,
-          price: 20000,
-          storeId: 1,
-          image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s'
-        },
-      ]
+      price: 20000,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_GOhUXbTBGraHVj2z0UFnXvCcTRBsY_hXBg&s',
+    },
+  ]
+
+  return allProducts.filter(product=> product.storeId == storeId);
 }
