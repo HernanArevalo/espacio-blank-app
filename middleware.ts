d05b01178f072383dcfa06a03dae432348aec6a6
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-08-31 13:50:39 -0300

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,21 @@
-export { auth as middleware } from "@/app/auth"
+// middleware.ts
+import { auth } from "@/app/auth"; // o "@/app/auth" según dónde tengas tu archivo
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const { pathname } = req.nextUrl;
+
+  const allowedPaths = ["/"]
+
+  if (!req.auth && !allowedPaths.includes(pathname) ) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|ico|webp)).*)",
+  ],
+};
