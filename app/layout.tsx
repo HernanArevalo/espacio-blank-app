b389c7a5a156995d1bce781513afac2fbf44f324
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-08-31 13:54:27 -0300

    format and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  
+
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="h-full flex flex-col">
         <Providers>
           <Navbar />
-          {children}
+          <main className="flex-1 min-h-0">{children}</main>
         </Providers>
       </body>
     </html>
