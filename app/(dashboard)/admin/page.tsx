d7f75257d60a7f46bf9e28231928a83fcf76d5f7
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-09-08 14:49:11 -0300

    delete admin middleware

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,26 +1,23 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { AdminPanel } from "@/components/admin-panel"
 import type { UserRole } from "@/lib/data"
+import { getUser } from "@/actions/user"
+import { useSession } from "next-auth/react"
 
 export default function AdminPage() {
-  const [userRole, setUserRole] = useState<UserRole | null>(null)
+  const { data:session} = useSession()
+  const user = getUser(session?.user || null)
   const router = useRouter()
 
   useEffect(() => {
-    const role = localStorage.getItem("userRole") as UserRole
-    if (!role || role !== "super_admin") {
+    if (!user.role || user.role !== "admin") {
       router.push("/")
       return
     }
-    setUserRole(role)
   }, [router])
-
-  if (!userRole || userRole !== "super_admin") {
-    return null
-  }
 
   return <AdminPanel />
 }
