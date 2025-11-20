f88f40a9e4fbc7ee53674e3303d7c5d6c5272fb0
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-11-19 23:21:01 -0300

    format document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from 'next'
-import '@/app/globals.css'
-import Navbar from '@/components/navbar'
-import Providers from './providers'
-
+import type { Metadata } from 'next';
+import '@/app/globals.css';
+import Navbar from '@/components/navbar';
+import Providers from './providers';
+import ClientLayout from './ClientLayout';
 
 export const metadata: Metadata = {
   title: {
@@ -11,22 +11,23 @@ export const metadata: Metadata = {
   },
   description: 'Espacio Blank',
   icons: "/favicon.ico"
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en" className="h-full">
       <body className="h-full flex flex-col">
         <Providers>
           <ClientLayout>
+            <Navbar />
+            <main className="flex-1 min-h-0">{children}</main>
           </ClientLayout>
         </Providers>
       </body>
     </html>
-  )
+  );
 }
