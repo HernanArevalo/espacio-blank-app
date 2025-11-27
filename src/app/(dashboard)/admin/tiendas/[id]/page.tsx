266d7b39390916a85d8109c04ab9a2a4704e58e1
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-11-27 14:20:17 -0300

    update tienda id page

--- src/app/(dashboard)/admin/tiendas/[id]/page.tsx
+++ src/app/(dashboard)/admin/tiendas/[id]/page.tsx
@@ -1,37 +1,61 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter, useParams } from "next/navigation"
 import { EditarTienda } from "@/components/editar-tienda"
-import { useSession } from "next-auth/react"
 import { getUser } from "@/actions/user"
 import { getStoreById } from "@/actions/store"
+import { Store } from "@/interfaces"
+import { useStore } from "@/store"
+import { useAuth } from "@/context/AuthContext"
 
 export default function EditarTiendaPage() {
   const router = useRouter()
   const params = useParams()
   const storeId = Number(params.id)
+  const [tienda, setTienda] = useState<Store|null>(null)
+  const { loading, setLoading } = useStore()
 
-  const { data:session} = useSession()
-  const user = getUser(session?.user || null)
+  const { user } = useAuth()
+
+  const getTienda = async() => {
+    const store = await getStoreById(storeId)
+    if (!store) {
+      router.push("/admin")
+      return null
+    }else{
+      setTienda(store)
+    }
+
+  }
 
   useEffect(() => {
+    setLoading(true)
+    getTienda()
+    
     if (!user.role || user.role !== "admin") {
       router.push("/")
       return
     }
-
+    setLoading(false)
   }, [router])
 
   if (!user.role || user.role !== "admin") {
     return null
   }
 
-  const tienda = getStoreById(storeId)
-  if (!tienda) {
-    router.push("/admin")
-    return null
+  if (loading) {
+    return (
+      <div className="h-full flex items-center justify-center bg-gray-50">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
+          <p className="text-slate-600">Cargando...</p>
+        </div>
+      </div>
+    )
   }
 
-  return <EditarTienda tienda={tienda} />
+  if (tienda) {
+    return <EditarTienda tienda={tienda} />
+  }
 }
