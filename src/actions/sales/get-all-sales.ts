afc1a72b5e28f89a7e1f6e4e63cce32f26fb9fbc
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-11-27 14:08:36 -0300

    add getAllSales action

--- src/actions/sales/get-all-sales.ts
+++ src/actions/sales/get-all-sales.ts
@@ -1,6 +1,13 @@
-import { sales } from "@/data";
+"use server";
 
+import { Sale } from "@/interfaces"
+import prisma from "@/lib/prisma"
 
-export function getAllSales () {
-  return sales
+export async function getAllSales(): Promise<Sale[]> {
+  try {
+    return await prisma.sale.findMany({ include: { items: true } })
+  } catch (error) {
+    console.error('Error fetching stores:', error)
+    return []
+  }
 }
