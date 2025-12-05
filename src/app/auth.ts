4b22b9d40b0929135677a641e4431dd58b89f07a
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-12-05 12:53:56 -0300

    update auth

---
+++ src/app/auth.ts
@@ -0,0 +1,81 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import prisma from "@/lib/prisma"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        try {
+          const email = user.email!
+          let dbUser = await prisma.user.findUnique({ where: { email } })
+
+          if (!dbUser) {
+            dbUser = await prisma.user.create({
+              data: {
+                email,
+                name: user.name || "user",
+                image: user.image,
+                role: "user",
+              },
+            })
+          }
+          
+          user.id = dbUser.id as unknown as string 
+          user.role = dbUser.role 
+          
+          return true
+        } catch (error) {
+          console.error("Error en signIn:", error)
+          return false
+        }
+      }
+      return true
+    },
+
+
+    async jwt({ token, user, trigger, session }) {
+      if (user) {
+        token.id = user.id as unknown as number
+        token.role = user.role
+      } 
+      else if (token.email) {
+        const dbUser = await prisma.user.findUnique({
+          where: { email: token.email },
+          select: { role: true, image: true, name: true }
+        })
+
+        if (dbUser) {
+          token.role = dbUser.role
+          token.picture = dbUser.image 
+          token.name = dbUser.name
+        }
+      }
+      
+      if (trigger === "update" && session) {
+        token = { ...token, ...session }
+      }
+
+      return token
+    },
+
+    // 3. SESSION (Igual que antes)
+    async session({ session, token }) {
+      if (session && session.user) {
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            id: token.id as any,
+            role: token.role,
+            image: token.picture, 
+            name: token.name
+          },
+        }
+      }
+      return session
+    },
+  },
+})
