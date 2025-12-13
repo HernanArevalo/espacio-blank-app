9d1dd4f6f979ea4e08f843f4ba5c4296c3751532
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-12-13 19:23:42 -0300

    delete background on loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="h-full flex items-center justify-center bg-gray-50">
+    <div className="h-full flex items-center justify-center">
       <div className="text-center">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
         <p className="text-slate-600">Cargando...</p>
