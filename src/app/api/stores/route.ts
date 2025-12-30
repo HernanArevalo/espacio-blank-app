46943f1c1a4464d82997388428344485b38f0e61
Hernán Arévalo <hernanarevalo16@gmail.com>
2025-12-30 13:04:30 -0300

    update restrictionjs in api routes

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -1,7 +1,7 @@
-"use server";
-
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const stores = await prisma.store.findMany({
