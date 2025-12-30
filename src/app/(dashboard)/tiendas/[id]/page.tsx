b0c79a21fce5dde40ee5300fa76796ef26712dfa
Hernán Arévalo <hernanarevalo16@gmail.com>
2025-12-30 13:50:04 -0300

    add force-dynamic

--- src/app/(dashboard)/tiendas/[id]/page.tsx
+++ src/app/(dashboard)/tiendas/[id]/page.tsx
@@ -4,6 +4,8 @@ import { TiendaPanel } from "@/components/tienda-panel"
 import { useAuth } from "@/context/AuthContext"
 import { redirect, } from "next/navigation"
 
+export const dynamic = "force-dynamic";
+
 export default function TiendaPage() {
   const { user } = useAuth()
 
