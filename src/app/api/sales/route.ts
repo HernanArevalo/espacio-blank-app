d3e214456cd1ec9d24dcae5365461dd0ce3489c0
Hernán Arévalo <hernanarevalo16@gmail.com>
2025-12-30 12:48:58 -0300

    update sale api

--- src/app/api/sales/route.ts
+++ src/app/api/sales/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET() {
-  const sales = await prisma.sale.findMany()
+export const dynamic = "force-dynamic";
 
+export async function GET() {
+  const sales = await prisma.sale.findMany();
   return NextResponse.json(sales);
 }
